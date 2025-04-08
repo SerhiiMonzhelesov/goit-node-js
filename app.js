9af69b1558578c0134011e4435cd4f4c05872333
SerhiiMonzhelesov <129993794+SerhiiMonzhelesov@users.noreply.github.com>
2025-04-08 17:16:05 +0300

    Changes

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const express = require("express");
-
-const moment = require("moment");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const { nanoid } = require("nanoid");
 const fs = require("fs/promises");
-
-// const contacts = require("./data/contacts");
-const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/api/contacts", contactsRouter);
+const tempDir = path.join(__dirname, "temp");
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
+
+const contacts = [];
+
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+const contactsDir = path.join(__dirname, "public", "contacts");
+
+app.post("/api/contacts", upload.single("photo"), async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const resultUpload = path.join(contactsDir, originalname);
+  await fs.rename(tempUpload, resultUpload);
+  const cont = path.join("contacts", originalname);
+  const newContact = {
+    id: nanoid(),
+    ...req.body,
+    cont,
+  };
+  contacts.push(newContact);
+
+  res.status(201).json(newContact);
+});
+
+// app.use("/api/contacts", contactsRouter);
 
 // app.use(async (req, res, next) => {
 //   const { method, url } = req;
@@ -20,13 +55,15 @@ app.use("/api/contacts", contactsRouter);
 //   next();
 // });
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Relax, everything is fine");
+});
